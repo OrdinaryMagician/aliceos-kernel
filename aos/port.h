56e5161785d7266a9db3a960d6209caa2032e78c
Marisa Kirisame <marisa@sayachan.org>
2012-12-14 13:51:02 +0100

    fuck this shit

--- aos/port.h
+++
@@ -1,7 +0,0 @@
-#ifndef PORT_H
-#define PORT_H
-#include "types.h"
-void outb( Uint16 port, Uint8 val );
-Uint8 inb( Uint16 port );
-Uint16 inw( Uint16 port );
-#endif
--- aos/timer.h
+++
@@ -1,5 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-#include "types.h"
-void init_timer( Uint32 frequency );
-#endif
