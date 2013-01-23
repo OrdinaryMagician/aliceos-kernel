523617395ab8833da0dfc74e7814827b3199e384
Marisa Kirisame <marisa@sayachan.org>
2013-01-23 18:07:08 +0100

    Why did I even have these

--- kernel/aos/vgagfx.c
+++
@@ -1,9 +0,0 @@
-/*
-	vgagfx.c : Standard VGA graphics functionality.
-	(C)2012-2013 Marisa Kirisame, UnSX Team.
-	Part of AliceOS, the Alice Operating System.
-	Released under the MIT License.
-*/
-#include <vgagfx.h>
-
-/* TODO */
