ccba425b4bf9ae2fbc215a5637641e987e3933a1
Marisa Kirisame <marisa@sayachan.org>
2013-02-18 10:16:40 +0100

    started animated image implementation

---
+++ kernel/aos/video/loadanim.c
@@ -0,0 +1,60 @@
+/*
+	loadanim.c : Animated Image loading.
+	(C)2012-2013 Marisa Kirisame, UnSX Team.
+	Part of AliceOS, the Alice Operating System.
+	Released under the MIT License.
+*/
+#include <video/loadanim.h>
+#include <fs/ramdisk.h>
+#include <memops.h>
+
+/* anm file magic (OS magic  file format magic) */
+Uint8 anm_magic[24] = 
+{
+	'A', 'L', 'I', 'C', 'E', 'O', 'S',
+	0xE3, 0x81, 0xA7, 0xE3, 0x82, 0x8C, /* Hiragana "dere", UTF-8 */
+	'R', 'A', 'W', 'A', 'N', 'M', 'F', 'I', 'L', 'E',
+	'\0',
+};
+/* anm file header structure */
+typedef struct
+{
+	Uint8 magic[24];
+	Uint8 flags;
+	Uint8 depth;
+	Uint16 width;
+	Uint16 height;
+	Uint16 palsz;
+} anm_header_t;
+/* header is followed by the palette (if available), then first frame */
+
+/* load an animation from the ramdisk */
+Uint8 loadanm( anm_t *dest, char *fname )
+{
+	/* there are many error checks here... */
+	if ( !dest )
+		return 1;
+	rd_entry_t *anm = rd_find(fname);
+	if ( !anm )
+		return 1;
+	if ( anm->size < sizeof(anm_header_t) )
+		return 1;
+	Uint8 *anmdata = (Uint8*)rd_find_data(fname);
+	/* verify header magic */
+	anm_header_t *hd = (anm_header_t*)anmdata;
+	if ( memcmp(&(hd->magic[0]),&anm_magic[0],24) )
+		return 1;
+	/* first frame retrieval */
+	anmdata += sizeof(anm_header_t);
+	if ( !(hd->flags&ANM_FL_NOPAL) )
+	{
+		dest->pal = anmdata;
+		anmdata += hd->palsz;
+	}
+	dest->frames = (anm_frame_t*)anmdata;
+	dest->w = hd->width;
+	dest->h = hd->height;
+	dest->flags = hd->flags;
+	dest->depth = hd->depth;
+	return 0;
+}
---
+++ kernel/include/video/loadanim.h
@@ -0,0 +1,51 @@
+/*
+	loadanim.h : Animated Image loading.
+	(C)2012-2013 Marisa Kirisame, UnSX Team.
+	Part of AliceOS, the Alice Operating System.
+	Released under the MIT License.
+*/
+#ifndef LOADANIM_H
+#define LOADANIM_H
+#include <sys/types.h>
+/* depth types */
+#define ANM_DP_MONO   0 /* 8bpp monochrome */
+#define ANM_DP_4COL   1 /* 8bpp 4 color */
+#define ANM_DP_16COL  2 /* 8bpp 16 color */
+#define ANM_DP_256COL 3 /* 8bpp 256 color */
+#define ANM_DP_RGB8   4 /* 24bpp RGB */
+#define ANM_DP_RGBA8  5 /* 32bpp RGBA */
+
+/* flag definitions (most reserved) */
+#define ANM_FL_NOPAL   1 /* no embedded palette */
+#define ANM_FL_ONESH   2 /* one-shot (doesn't loop) */
+#define ANM_FL_RESV3   4
+#define ANM_FL_RESV4   8
+#define ANM_FL_RESV5  16
+#define ANM_FL_RESV6  32
+#define ANM_FL_RESV7  64
+#define ANM_FL_RESV8 128
+
+/* struct for each anim frame */
+typedef struct
+{
+	Uint16 index;
+	Uint16 next;
+	Uint32 duration;
+	Uint32 frameloc;
+	Uint32 framesize;
+} anm_frame_t;
+
+/* struct for an animation */
+typedef struct
+{
+	anm_frame_t *frames;
+	Uint8 *pal;
+	Uint16 w;
+	Uint16 h;
+	Uint8 flags;
+	Uint8 depth;
+} anm_t;
+
+/* load an animation from the ramdisk */
+Uint8 loadanm( anm_t *dest, char *fname );
+#endif
